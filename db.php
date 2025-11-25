db419935268cfed7dbea713d4e5bb84ef5118fbd
Dilhara Jayawardhana <dilhara5287@gmail.com>
2025-11-26 02:23:16 +0530

    logo added to the header

--- db.php
+++ db.php
@@ -12,7 +12,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Remove the echo statement - you don't want this showing on every page
-// echo "Connected successfully";
-
-// Don't close the connection here - let the including script manage it
+?>
